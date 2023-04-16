5a3e331369e4e4915a33d14f94b7b6e0ed875e77
xlivia <oliviabryanttt@gmail.com>
2023-04-16 11:16:45 -0400

    psql_docker

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -1,57 +1,25 @@
 #!/bin/bash
 
-#capture CLI arguments (please do not copy comments)
-cmd=$1
-db_username=$2
-db_password=$3
+# get latest postgres image
+docker pull postgres
 
-#Start docker
-#Make sure you understand `||` cmd
-docker info > /dev/null 2>&1 || start-service docker
+# create a new volume if not exist
+docker volume create pgdata
 
-#check container status (try the following cmds on terminal)
-docker inspect jrvs-psql > /dev/null 2>&1
-container_status=$?
+# create a container using psql image with name=jrvs-psql
+docker run --name jrvs-psql -e POSTGRES_PASSWORD=password -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
 
-#User switch case to handle create|stop|start opetions
-case $cmd in 
-  create)
-  
-  # Check if the container is already created
-  if [ $container_status -eq 0 ]; then
-		echo 'Container already exists'
-		exit 1	
-	fi
+# check if the container `jrvs-psql` is created or not
+docker container ls -a -f name=jrvs-psql
 
-  #check # of CLI arguments
-  if [ $# -ne 3 ]; then
-    echo 'Create requires username and password'
-    exit 1
-  fi
-  
-  #Create container
-	docker volume create pgdata
-  #Start the container
-	docker run --name jrvs-psql -e POSTGRES_PASSWORD=$db_password -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
-  #Make sure you understand what's `$?`
-	exit $?
-	;;
+# check if `jrvs-psql` container is running
+docker ps -f name=jrvs-psql
 
-  start|stop) 
-  #check instance status; exit 1 if container has not been created
-  if [ $container_status -ne 0 ]; then
-		echo 'Container does not exist'
-		exit 1	
-	fi
+# If the container is not running, start it by running the command
+docker start jrvs-psql
 
-  #Start or stop the container
-	docker container $cmd jrvs-psql
-	exit $?
-	;;	
-  
-  *)
-	echo 'Illegal command'
-	echo 'Commands: start|stop|create'
-	exit 1
-	;;
-esac
+# Set the password for the default user postgres by running the command:
+export PGPASSWORD='password'
+
+# Connect to the psql instance using the psql REPL (read-eval-print loop) by running the command
+psql -h localhost -U postgres -d postgres -W
