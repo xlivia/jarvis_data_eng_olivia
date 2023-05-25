df724cfb9871529f1b00eb01ccb53edb8bb2aa98
xlivia <oliviabryanttt@gmail.com>
2023-05-25 10:31:05 -0400

    coding_challenges

--- coding_challenges/FindDuplicateNum.java
+++ coding_challenges/FindDuplicateNum.java
@@ -1,5 +1,6 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -7,8 +8,9 @@ class FindDuplicateNum {
 
     public static int[] readIntArray(Scanner scanner) {
         while (true) {
+            System.out.print("Enter the numbers separated by commas: ");
+            String input = scanner.nextLine();
             try {
-                String input = scanner.nextLine();
                 String[] parts = input.split(",");
                 int[] nums = new int[parts.length];
                 for (int i = 0; i < parts.length; i++) {
@@ -17,28 +19,38 @@ class FindDuplicateNum {
                 return nums;
             } catch (NumberFormatException e) {
                 System.out.println("Invalid input. Please enter comma-separated integers only.");
-                System.out.print("Enter the numbers separated by commas: ");
             }
         }
     }
 
-    public static int findDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
-                return nums[i];
+    public static List<Integer> findDuplicates(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        List<Integer> duplicates = new ArrayList<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                duplicates.add(num);
             }
+            set.add(num);
         }
-        throw new IllegalArgumentException("No duplicate number found.");
+        if (duplicates.isEmpty()) {
+            throw new IllegalArgumentException("No duplicate numbers found.");
+        }
+        return duplicates;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the numbers separated by commas: ");
         int[] nums = readIntArray(scanner);
         try {
-            int duplicate = findDuplicate(nums);
-            System.out.println("Duplicate number: " + duplicate);
+            List<Integer> duplicates = findDuplicates(nums);
+            StringBuilder output = new StringBuilder("Duplicate number(s): ");
+            for (int i = 0; i < duplicates.size(); i++) {
+                output.append(duplicates.get(i));
+                if (i != duplicates.size() - 1) {
+                    output.append(", ");
+                }
+            }
+            System.out.println(output.toString());
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
