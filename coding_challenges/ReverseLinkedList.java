3e9b1074d93e7327563f79f66638f3d249030e9d
xlivia <oliviabryanttt@gmail.com>
2023-05-26 11:15:54 -0400

    coding_challenges

---
+++ coding_challenges/ReverseLinkedList.java
@@ -0,0 +1,43 @@
+public class ReverseLinkedList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) {
+            this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ReverseLinkedList solution = new ReverseLinkedList();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        ListNode reversedHead = solution.reverseList(head);
+        System.out.print("Reversed List: ");
+        while (reversedHead != null) {
+            System.out.print(reversedHead.val + " ");
+            reversedHead = reversedHead.next;
+        }
+        System.out.println();
+    }
+
+}
