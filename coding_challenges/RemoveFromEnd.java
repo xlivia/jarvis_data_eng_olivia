2bcad0a4d4fd5bd6a4ce59fe682fa77f418794f6
xlivia <oliviabryanttt@gmail.com>
2023-05-26 10:07:48 -0400

    coding_challenges

--- coding_challenges/RemoveFromEnd.java
+++ coding_challenges/RemoveFromEnd.java
@@ -2,64 +2,49 @@ class RemoveFromEnd {
     static class ListNode {
         int val;
         ListNode next;
-
+        ListNode() {}
         ListNode(int val) {
             this.val = val;
         }
-    }
-
-    public static ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head == null || n <= 0) {
-            return head;
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
         }
-
-        ListNode dummy = new ListNode(0);
+    }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0); // Create a dummy node
         dummy.next = head;
-        ListNode slow = dummy;
         ListNode fast = dummy;
-
+        ListNode slow = dummy;
         // Move the fast pointer n steps ahead
         for (int i = 0; i <= n; i++) {
-            if (fast == null) {
-                return head;
-            }
             fast = fast.next;
         }
-
-        // Move the slow and fast pointers simultaneously
+        // Move fast and slow pointers simultaneously
         while (fast != null) {
-            slow = slow.next;
             fast = fast.next;
+            slow = slow.next;
         }
-
         // Remove the nth node from the end
         slow.next = slow.next.next;
-
         return dummy.next;
     }
-
     public static void main(String[] args) {
         // Create a sample linked list: 1 -> 2 -> 3 -> 4 -> 5
-        ListNode head = new ListNode(1);
-        ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(3);
-        ListNode node4 = new ListNode(4);
         ListNode node5 = new ListNode(5);
-
-        head.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
+        RemoveFromEnd solution = new RemoveFromEnd();
         int n = 2;
-
-        // Remove the nth node from the end
-        ListNode result = removeNthFromEnd(head, n);
-
+        ListNode result = solution.removeNthFromEnd(node1, n);
         // Print the modified linked list
-        while (result != null) {
-            System.out.print(result.val + " ");
-            result = result.next;
+        ListNode current = result;
+        while (current != null) {
+            System.out.print(current.val + " -> ");
+            current = current.next;
         }
+        System.out.println("null");
     }
 }
